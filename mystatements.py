217515a6632bbdbd182b82d11635e89496027f1c
LeninKatta <108182396+LeninKatta45@users.noreply.github.com>
2024-06-13 17:09:57 +0000

    adding files to repo

---
+++ mystatements.py
@@ -0,0 +1,10 @@
+list=["apple","cherry","lemon"]
+dict={"drink":"milk","1":"2"}
+
+for i in list:
+    print(f"my food: {i}")
+for _,value in dict.items():
+    print(f"my favorite: {value}")
+
+#dict=dict
+#list2=
